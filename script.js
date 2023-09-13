845599954005a95cd221446ea09b5ff1c4fa6660
shuklaadity <aditya02shukla1@gmail.com>
2023-09-13 22:40:34 +0530

    add new reposetery

---
+++ script.js
@@ -0,0 +1,13 @@
+var manuicon= document.querySelector(".manuicon");
+var sidebar=document.querySelector(".sidebar");
+var manubaricon=document.querySelector(".sidebar-1-1-manubar");
+
+manuicon.onclick= function()
+{
+    sidebar.classList.toggle("small-sidebar");
+}
+manubaricon.onclick= function()
+{
+    sidebar.classList.toggle("small-sidebar");
+}
+
